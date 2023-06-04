b0ab9fb7483a9be2ea50d73b9a67fc78c6a67517
gothdawid <dawidstu@gmail.com>
2023-06-04 18:11:57 +0200

    firewall end

--- router/firewallRouter.js
+++ router/firewallRouter.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { execSync } = require("child_process");
+const { execSync, exec } = require("child_process");
 
 // Ścieżka do ustawień firewalla
 router.get("/", (req, res) => {
@@ -16,11 +16,14 @@ router.get("/", (req, res) => {
   });
 });
 
-// Obsługa zapisu ustawień firewalla
+// Obsługa formularza firewalla
 router.post("/", (req, res) => {
-  const { enable, nat, securityLevel, sshAccess, customRules } = req.body;
+  const { enable, nat, tcpPort, udpPort } = req.body;
 
-  // Wykonaj odpowiednie operacje zapisu ustawień firewalla
+  // Zapisz ustawienia firewalla
+  setFirewallNatEnabled(nat === "on");
+  setOpenPorts(tcpPort, udpPort);
+  setFirewallEnabled(enable === "on");
 
   res.redirect("/firewall");
 });
@@ -31,14 +34,23 @@ function isFirewallEnabled() {
   return output.includes("Status: Enabled");
 }
 
+// Funkcja pomocnicza do ustawiania włączenia/wyłączenia firewalla
+function setFirewallEnabled(enabled) {
+  const firewallStatus = isFirewallEnabled();
+  if (enabled && !firewallStatus) {
+    const configStatus = execSync("pfctl -nf /etc/pf.conf");
+    if (configStatus.toString() === "") {
+      execSync("pfctl -f /etc/pf.conf");
+      execSync("pfctl -e");
+    }
+  }
+  if (!enabled && firewallStatus) {
+    execSync("pfctl -d");
+  }
+}
+
 // Funkcja pomocnicza do sprawdzania czy NAT jest włączony
 function isFirewallNatEnabled() {
-  // jeśli plik /etc/pf.conf zawiera wpis
-  // match in all scrub (no-df random-id max-mss 1440)
-  // match out on em0 inet from !(em0:network) to any nat-to (em0)
-  // to NAT jest włączony
-  // jeśli przed tymi niniami jest # to jest wyłączony
-
   const output = execSync("cat /etc/pf.conf").toString().split("\n");
   let natEnabled = 0;
 
@@ -63,35 +75,95 @@ function isFirewallNatEnabled() {
     }
   });
 
-  let Enabled = false;
-  if (natEnabled == 3) {
-    Enabled = true;
-  }
-
-  return Enabled;
+  return natEnabled === 3;
 }
 
-// Funkcja pomocnicza do pobierania dostępu SSH
-function getOpenPorts() {
-  // pass in on $wan proto tcp from any to any port { ssh, https, http, ftp, sftp, 3000 } keep state
-  // pass in on $wan proto udp from any to any port { domain, ntp, http, 3000 } keep state
+// Funkcja pomocnicza do ustawiania włączenia/wyłączenia NAT
+function setFirewallNatEnabled(enabled) {
+  const natStatus = isFirewallNatEnabled();
+  let pfConf = execSync("cat /etc/pf.conf").toString();
+  pfConf = pfConf.replaceAll("$", "\\$");
 
+  if (enabled && !natStatus) {
+    pfConf = pfConf.replace(
+      "#match in all scrub (no-df random-id max-mss 1440)",
+      "match in all scrub (no-df random-id max-mss 1440)"
+    );
+    pfConf = pfConf.replace(
+      "#match out on em0 inet from !(em0:network) to any nat-to (em0)",
+      "match out on em0 inet from !(em0:network) to any nat-to (em0)"
+    );
+  }
+  if (!enabled && natStatus) {
+    pfConf = pfConf.replace(
+      "match in all scrub (no-df random-id max-mss 1440)",
+      "#match in all scrub (no-df random-id max-mss 1440)"
+    );
+    pfConf = pfConf.replace(
+      "match out on em0 inet from !(em0:network) to any nat-to (em0)",
+      "#match out on em0 inet from !(em0:network) to any nat-to (em0)"
+    );
+  }
+  execSync(`echo "${pfConf}" > /etc/pf.conf`);
+  //execSync("pfctl -f /etc/pf.conf");
+}
+
+// Funkcja pomocnicza do ustawiania otwartych portów
+function setOpenPorts(tcpPorts, udpPorts) {
+  let pfConf = execSync("cat /etc/pf.conf").toString();
+
+  pfConf = pfConf.replaceAll("$", "\\$");
+  const tcpPattern = /.*proto tcp from any to any port { (.*?) }.*/g;
+  const udpPattern = /.*proto udp from any to any port { (.*?) }.*/g;
+
+  const tcpMatch = tcpPattern.exec(pfConf);
+  const udpMatch = udpPattern.exec(pfConf);
+
+  console.log(tcpMatch[0]);
+  console.log(udpMatch[0]);
+
+  const tcpPortString = tcpPorts.join(", ");
+  const udpPortString = udpPorts.join(", ");
+
+  pfConf = pfConf.replace(
+    tcpPattern,
+    `pass in on \\$wan proto tcp from any to any port { ${tcpPortString.trim()} }`
+  );
+  pfConf = pfConf.replace(
+    udpPattern,
+    `pass in on \\$wan proto udp from any to any port { ${udpPortString.trim()} }`
+  );
+
+  //save file to /etc/pf.conf
+  //console.log(`${pfConf}`);
+  //console.log(`echo "${pfConf}`);
+  execSync(`echo "${pfConf}" > /etc/pf.conf`);
+  //execSync("pfctl -f /etc/pf.conf");
+}
+// Funkcja pomocnicza do pobierania otwartych portów
+function getOpenPorts() {
   const output = execSync("cat /etc/pf.conf").toString().split("\n");
   let openPorts = {
     tcp: [],
-    udp: []
+    udp: [],
   };
+
   output.forEach((line) => {
     if (line.includes("pass in on $wan proto tcp from any to any port")) {
-      openTCPports = line.split("{ ")[1].split(" }")[0].split(", ");
-      openPorts.tcp = openTCPports;
+      openPorts.tcp = extractPortsFromLine(line);
     }
     if (line.includes("pass in on $wan proto udp from any to any port")) {
-      openUDPports = line.split("{ ")[1].split(" }")[0].split(", ");
-      openPorts.udp = openUDPports;
+      openPorts.udp = extractPortsFromLine(line);
     }
   });
+
   return openPorts;
+}
+
+// Funkcja pomocnicza do ekstrakcji portów z linii
+function extractPortsFromLine(line) {
+  const portString = line.split("{")[1].split("}")[0];
+  return portString.split(", ").filter((port) => port !== "");
 }
 
 module.exports = router;
